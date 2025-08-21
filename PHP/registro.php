6f4d686b3ec38bed92a061b65a4c867c09a0b09f
Alexander Ocampo <alexanderocaher@gmail.com>
2025-08-21 17:47:22 +0200

    Se agrega seguridad al formulario para evitar inyecciones SQL

--- PHP/registro.php
+++ PHP/registro.php
@@ -3,17 +3,19 @@ include_once('conection.php');
 
 // Verificar si se envió el formulario
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Extracción datos del formulario
-    $name = $_POST['name'];
-    $surname = $_POST['surname'];
-    $email = $_POST['email'];
-    $pass = $_POST['pass'];
+    // Extracción de datos del formulario
+    $name = htmlspecialchars($_POST['name']);
+    $surname = htmlspecialchars($_POST['surname']);
+    $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+    $pass = htmlspecialchars($_POST['pass']);
 
-    // Comprobar si el usuario ya está registrado
-    $userExists = "SELECT Names FROM users WHERE email='$email'";
-    $validateEmail = mysqli_query($connection, $userExists);
+    // Comprobar si el usuario ya está registrado usando consulta preparada
+    $userExists = "SELECT Names FROM users WHERE email=?";
+    $stmt = $connection->prepare($userExists);
+    $stmt->bind_param("s", $email);
+    $stmt->execute();
+    $validateEmail = $stmt->get_result();
 
-  
     ob_start();
     ?>
     <!DOCTYPE html>
@@ -21,7 +23,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Registro</title>       
+        <title>Registro</title>
         <link rel="stylesheet" href="../css/index.css">
     </head>
     <body>
@@ -36,11 +38,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     echo "<div class='error-message'>Ya existe el usuario. <a href='javascript:history.back()'>Volver</a></div>";
                 }
                 else{
-                    // Enviar datos a la base de datos
-                    $data = "INSERT INTO users (Names, Surname, Email, Pass) VALUES ('$name', '$surname', '$email', '$pass')";
-                    $validate = mysqli_query($connection, $data);
+                    // Preparar la consulta para insertar un nuevo usuario usando consulta preparada
+                    $data = "INSERT INTO users (Names, Surname, Email, Pass) VALUES (?, ?, ?, ?)";
+                    $insertStmt = $connection->prepare($data);
+                    $insertStmt->bind_param("ssss", $name, $surname, $email, $pass);
 
-                    if($validate != false){
+                    if($insertStmt->execute()){
                         // Redirigir a la página de inicio si el registro es exitoso
                         header('Location:../public/inicio.html');
                         exit;
@@ -59,8 +62,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     </body>
     </html>
     <?php
-    echo ob_get_clean(); 
-    exit; 
+    echo ob_get_clean();
+    exit;
 }
 ?>
-
